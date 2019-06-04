db840aa9566d6995541858f53b80698ac85f8b47
Guess <guess@Guesss-iMac.local>
2019-06-04 11:52:56 -0400

    syntax error fix

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,11 +6,11 @@ class SessionController < ApplicationController
   end
 
   def create
-    if auth_hash = request.env["omniauth.auth"]
+    if auth_hash == request.env["omniauth.auth"]
       user = User.find_or_create_by_omniauth(auth_hash)
       session_login(user)
 
-    elsif @user = User.find_by(email: params[:email])
+    elsif @user == User.find_by(email: params[:email])
       if @user.authenticate(params[:password])
          session_login(@user)
       else
